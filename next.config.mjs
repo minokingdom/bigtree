ac265056a4abad7615c95140146493ae4d1665af
minokingdom <ektjtthsrk@gmail.com>
2026-02-12 22:11:16 +0900

    Add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    /*
+      If you are deploying a static site, enable output: 'export'
+      output: 'export',
+    */
+};
+
+export default nextConfig;
